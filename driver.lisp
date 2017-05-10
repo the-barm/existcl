e7aa9c87bebad24bf39ad009679e9b7f0980e2ac
the-barm <funfundzwanzigsten@gmail.com>
2017-05-10 07:39:47 +0300

    small fixes & added documentation functions

--- driver.lisp
+++ driver.lisp
@@ -41,6 +41,10 @@
         ((string-equal arg "false") nil)
         (t (error "The value of: ~A is not of boolean type" arg))))
 
+(defun ignore-warning (condition)
+   (declare (ignore condition))
+   (muffle-warning))
+
 (defmacro with-gensyms (syms &body body)
   `(let ,(mapcar #'(lambda (s) `(,s (gensym)))
                  syms)
@@ -86,15 +90,17 @@ or setup it using (make-config :address \"youraddress\" :port (by default is \"8
              nil)))))
 
 (defun parse-resulting-xml (target &key can-be-null multiple)
-  (if (xmls:xmlrep-children (xmls:parse target))
-      (if multiple
-          (xmls:xmlrep-find-child-tags "value"
-                                      (xmls:parse target))
-          (xmls:xmlrep-find-child-tag "value"
-                                      (xmls:parse target)))
-      (if can-be-null
-          nil
-          (error "Requested address is not found"))))
+  (if target
+      (if (xmls:xmlrep-children (xmls:parse target))
+          (if multiple
+              (xmls:xmlrep-find-child-tags "value"
+                                           (xmls:parse target))
+              (xmls:xmlrep-find-child-tag "value"
+                                          (xmls:parse target)))
+          (if can-be-null
+              nil
+              (error "Requested address is not found")))
+      (error "Query returned null")))
 
 (defun make-request-parameters (&key query indent encoding howmany start wrap source cache session release)
 			 (let ((params))
@@ -126,6 +132,7 @@ or setup it using (make-config :address \"youraddress\" :port (by default is \"8
 
 ;;INTERFACE FUNCTIONS
 (defun read-db-config ()
+  "Create new config instance with parameters specified by db.config file inside driver root directory"
   (with-open-file (stream "db.config")
   (let ((address (prepare-parameter "address" (read-line stream)))
         (port (prepare-parameter "port" (read-line stream)))
@@ -135,10 +142,13 @@ or setup it using (make-config :address \"youraddress\" :port (by default is \"8
 ;;(read-db-config)
 
 (defun make-config (&key address (port "8080") username password)
+  "Create new config instance from specified parameters"
      (setf *connection* (make-instance 'connection :address address :port port :username username :password password)))
 ;;(make-config :address "localhost" :port "8080" :username "admin" :password "admin")
 
 (defmacro get-document (address)
+  "Get document from the database. If MIME-type of the document allows it to be converted to string - it will be converted,
+otherwise the document is returned as a sequence of octets"
   `(with-drakma-http-request ,address :get :return-text t))
 ;;(get-document "shakespeare/hamlet.xml")
 ;;is equal to
@@ -148,19 +158,17 @@ or setup it using (make-config :address \"youraddress\" :port (by default is \"8
 ;;                         :basic-authorization '("admin" "admin"))
                    
 (defmacro put-xml-document (address content)
+  "Create XML document containing $content with address (including it's name) $address"
   `(with-drakma-http-request ,address :put :content-type "text/xml" :content ,(file-to-string content)))
 
-;;(put-xml-document "kkka/mydoc.xml" "/home/the-barm/workspace/existcl/mdoc.xml")
-
 (defmacro put-document (address content)
+  "Create plain-text document containing $content with address (including it's name) $address"
   `(with-drakma-http-request ,address :put :content-type "text/plain" :content ,(file-to-string content)))
 
-;;(put-document "kkka/mydoc.xml" "/home/the-barm/workspace/existcl/mdoc.xml")
-
 (defmacro put-document-from-string (address content-type content)
+  "Create document containing $content with address (including it's name) $address and MIME-type specified by $content-type"
   `(with-drakma-http-request ,address :put :content-type ,content-type :content ,content))
 
-;;(put-document-from-string "mycol2/mdoc.xml" "text/plain" "<test>test</test>")
 ;;(put-document-from-string "mycol2/mdoc.xml" "application/octet-stream" "<test>test</test>")
 ;;is equal to
 ;;(drakma:http-request "http://localhost:8080/exist/rest/db/mycol2/mdoc.xml"
@@ -169,72 +177,173 @@ or setup it using (make-config :address \"youraddress\" :port (by default is \"8
 ;;                         :basic-authorization '("admin" "admin"))
 
 (defmacro create-collection (address name)
+  "Create a new collection with name $name as a child of $address collection."
   `(execute-query ,address ,`(concatenate 'string "xmldb:create-collection('/db/" ,address "', '" ,name "')")))
 
-;;(create-collection "mycol2" "testcol")
-;;is equal to 
-;;(drakma:http-request "http://localhost:8080/exist/rest/db"
-;;                         :method :get
-;;                         :basic-authorization '("admin" "admin")
-;;                         :parameters '(("_query" . "xmldb:create-collection('mycol2', 'testcol')")))
-
 (defmacro delete-from-db (address)
+  "Removes specified recourse or collection (with all contents) from the database."
   `(with-drakma-http-request ,address :delete))
-
-;;(delete-from-db "mycol2/rararar")
 						      
 (defmacro move-collection (source direction)
+  "Moves the $source collection to the $direction collection"
   `(execute-query ,source ,`(concatenate 'string "xmldb:move('/db/" ,source "', '/db/" ,direction "')")))
-
-;;(move-collection "kkka/123" "mycol2")
 						      
 (defmacro move-document (source direction)
+  "Moves the $source resource to the $direction collection"
   (with-gensyms (src document)
     `(multiple-value-bind (,src ,document)
          (divide-path ,source)
        (execute-query ,source ,`(concatenate 'string "xmldb:move('/db/" ,src "', '/db/" ,direction "', '" ,document "')")))))
-
-;;(move-document "kkka/kkk.xml" "mycol2")
 					  
 (defmacro copy-collection (source direction)
+  "Copies the $source collection to the $direction collection"
   `(execute-query ,source ,`(concatenate 'string "xmldb:copy('/db/" ,source "', '/db/" ,direction "')")))
-
-;;(copy-collection "kkka/123" "mycol2")
 						      
 (defmacro copy-document (source direction)
+  "Copies the $source resource to the $direction collection"
   (with-gensyms (src document)
     `(multiple-value-bind (,src ,document)
          (divide-path ,source)
        (execute-query ,source ,`(concatenate 'string "xmldb:copy('/db/" ,src "', '/db/" ,direction "', '" ,document "')")))))
-
-;;(copy-document "kkka/kkk.xml" "mycol2")
-
 					  
-(defmacro rename-collection (path new-name)
-  `(execute-query ,path ,`(concatenate 'string "xmldb:rename('/db/" ,path "', '" ,new-name "')")))
-;;(rename-collection "adasd" "kkka")
+(defmacro rename-collection (source new-name)
+  "Renames the collection $source with new name $new-name."
+  `(execute-query ,source ,`(concatenate 'string "xmldb:rename('/db/" ,source "', '" ,new-name "')")))
 						      
-(defmacro rename-document (path new-name)
+(defmacro rename-document (source new-name)
+  "Renames the document $source with new name $new-name."
   (with-gensyms (src document)
     `(multiple-value-bind (,src ,document)
-         (divide-path ,path)
-       (execute-query ,path ,`(concatenate 'string "xmldb:rename('/db/" ,src "', '" ,document "', '" ,new-name "')")))))
-;;(rename-document "adasd/mdo.xml" "kkka.xml")
+         (divide-path ,source)
+       (execute-query ,source ,`(concatenate 'string "xmldb:rename('/db/" ,src "', '" ,document "', '" ,new-name "')")))))
 					  
 (defmacro execute-query (source query &key return-text)
+  "Runs query specified by $query string on $source address (accepts empty string) \n
+keys parameters:
+:return-text - if specified query returns it's output as XML-string, otherwise it returns t/nil depending on whether operation was successful"
   `(with-drakma-http-request ,source :get :parameters 
                              (make-request-parameters :query ,query) :return-text ,return-text))
+	      
+(defmacro get-collection-permissions (address)
+  "Returns the permissions assigned to the collection at $address "
+  `(xmls:xmlrep-integer-child
+    (parse-resulting-xml
+     (execute-query ,address (concatenate 'string "xmldb:get-permissions('" ,address "')") :return-text t))))
 
+(defmacro get-document-permissions (address)
+  "Returns the permissions assigned to the document at $address "
+  (with-gensyms (src document)
+    `(multiple-value-bind (,src ,document)
+         (divide-path ,address)
+       (xmls:xmlrep-integer-child
+        (parse-resulting-xml
+         (execute-query ,address (concatenate 'string "xmldb:get-permissions('" ,src "', '" ,document "')") :return-text t))))))
+
+(defmacro collection-available (address)
+  "Returns t if the collection at $address exists and is available, otherwise nil"
+  `(translate-boolean
+    (xmls:xmlrep-string-child
+     (parse-resulting-xml
+      (execute-query ,address (concatenate 'string "xmldb:collection-available('" ,address "')") :return-text t)))))
+
+(defmacro collection-created (address)
+  "Returns the creation date of the collection at $address."
+  `(xmls:xmlrep-string-child
+    (parse-resulting-xml
+     (execute-query ,address (concatenate 'string "xmldb:created('" ,address "')") :return-text t))))
+
+(defmacro document-created (address)
+  "Returns the creation date of the document at $address. "
+  (with-gensyms (src document)
+    `(multiple-value-bind (,src ,document)
+         (divide-path ,address)
+       (xmls:xmlrep-string-child
+        (parse-resulting-xml
+         (execute-query ,address (concatenate 'string "xmldb:created('" ,src "', '" ,document "')") :return-text t))))))
+
+(defmacro document-has-lock (address)
+  "Returns the user-id of the user that holds a write lock on the resource at $address."
+  (with-gensyms (src document)
+    `(multiple-value-bind (,src ,document)
+         (divide-path ,address)
+       (awhen (parse-resulting-xml
+               (execute-query ,address (concatenate 'string "xmldb:document-has-lock('" ,src "', '" ,document "')") :return-text t)
+               :can-be-null t)
+         (xmls:xmlrep-string-child it)))))
+
+(defmacro clear-lock (address)
+  "Clears lock on the document specified by $address and returns the user-id of the user that used to hold it."
+  (with-gensyms (src document)
+    `(multiple-value-bind (,src ,document)
+         (divide-path ,address)
+       (awhen (parse-resulting-xml
+               (execute-query ,address (concatenate 'string "xmldb:clear-lock('" ,src "', '" ,document "')") :return-text t)
+               :can-be-null t)
+         (xmls:xmlrep-string-child it)))))
+
+(defmacro get-child-collections (address)
+  "Returns the names of the child collections in the collection at $address. "
+  `(mapcar #'xmls:xmlrep-string-child
+           (parse-resulting-xml
+            (execute-query ,address (concatenate 'string "xmldb:get-child-collections('" ,address "')") :return-text t)
+            :multiple t)))
+
+(defmacro get-child-resources (address)
+  "Returns the names of the child resources in collection at $address. "
+  `(mapcar #'xmls:xmlrep-string-child
+           (parse-resulting-xml
+            (execute-query ,address (concatenate 'string "xmldb:get-child-resources('" ,address "')") :return-text t)
+            :multiple t)))
+
+(defmacro last-modified (address)
+  "Returns the last-modification date of resource at $address."
+  (with-gensyms (src document)
+    `(multiple-value-bind (,src ,document)
+         (divide-path ,address)
+       (xmls:xmlrep-string-child
+        (parse-resulting-xml
+         (execute-query ,address (concatenate 'string "xmldb:last-modified('" ,src "', '" ,document "')") :return-text t))))))
+
+(defmacro size (address)
+  "Returns the estimated size of the resource at $address (in bytes). 
+The estimation is based on the number of pages occupied by the resource. If the document is serialized back to a string, 
+its size may be different, since parts of the structural information are stored in compressed form. "
+  (with-gensyms (src document)
+    `(multiple-value-bind (,src ,document)
+         (divide-path ,address)
+       (xmls:xmlrep-integer-child
+        (parse-resulting-xml
+         (execute-query ,address (concatenate 'string "xmldb:size('" ,src "', '" ,document "')") :return-text t))))))
+
+(defmacro reindex (address)
+  "Reindex collection at $address. The XQuery owner must have appropriate privileges to do this, e.g. having DBA role."
+  `(translate-boolean
+    (xmls:xmlrep-string-child
+     (parse-resulting-xml
+      (execute-query ,address (concatenate 'string "xmldb:reindex('" ,address "')") :return-text t)))))
+
+(defmacro get-document-owner (address)
+  "Returns the owner user-id of the document at $address."
+  (with-gensyms (src document)
+    `(multiple-value-bind (,src ,document)
+         (divide-path ,address)
+       (xmls:xmlrep-string-child
+        (parse-resulting-xml
+         (execute-query ,address (concatenate 'string "xmldb:get-owner('" ,src "', '" ,document "')") :return-text t))))))
+
+(defmacro get-collection-owner (address)
+  "Returns the owner user-id of the collection at $address."
+  `(xmls:xmlrep-string-child
+    (parse-resulting-xml
+     (execute-query ,address (concatenate 'string "xmldb:get-owner('" ,address "')") :return-text t))))
 
 ;; tests
-(defun ignore-warning (condition)
-   (declare (ignore condition))
-   (muffle-warning))
 
 (defun test-interface-functions (tests)
+  "Small test-runner which accepts sequence of commands in format (((command) result) ...)"
   (handler-bind ((warning #'ignore-warning))
     (loop for test in tests
-       if (equalp (eval (first test)) (second test))
+       if (equalp (eval (first test)) (eval (second test)))
        do (format t "~A *done*~%" (caar test))
        else do (format t "~A #failed#~%" (caar test)))))
 
@@ -283,97 +392,21 @@ or setup it using (make-config :address \"youraddress\" :port (by default is \"8
                             ((rename-collection "wrongwrong" "123") nil)
                             ((move-collection "123" "testcol") nil)
                             ((delete-from-db "wrongwrong") nil)))
-	      
-(defmacro get-permissions (address)
-  `(xmls:xmlrep-integer-child
-    (parse-resulting-xml
-     (execute-query ,address (concatenate 'string "xmldb:get-permissions('" ,address "')") :return-text t))))
 
-(defmacro collection-available (address)
-  `(translate-boolean
-    (xmls:xmlrep-string-child
-     (parse-resulting-xml
-      (execute-query ,address (concatenate 'string "xmldb:collection-available('" ,address "')") :return-text t)))))
-
-(defmacro collection-created (address)
-  `(xmls:xmlrep-string-child
-    (parse-resulting-xml
-     (execute-query ,address (concatenate 'string "xmldb:created('" ,address "')") :return-text t))))
-
-(defmacro document-created (address)
-  (with-gensyms (src document)
-    `(multiple-value-bind (,src ,document)
-         (divide-path ,address)
-       (xmls:xmlrep-string-child
-        (parse-resulting-xml
-         (execute-query ,address (concatenate 'string "xmldb:created('" ,src "', '" ,document "')") :return-text t))))))
-
-(defmacro document-has-lock (address)
-  (with-gensyms (src document)
-    `(multiple-value-bind (,src ,document)
-         (divide-path ,address)
-       (awhen (parse-resulting-xml
-               (execute-query ,address (concatenate 'string "xmldb:document-has-lock('" ,src "', '" ,document "')") :return-text t)
-               :can-be-null t)
-         (xmls:xmlrep-string-child it)))))
-
-(defmacro clear-lock (address)
-  (with-gensyms (src document)
-    `(multiple-value-bind (,src ,document)
-         (divide-path ,address)
-       (awhen (parse-resulting-xml
-               (execute-query ,address (concatenate 'string "xmldb:clear-lock('" ,src "', '" ,document "')") :return-text t)
-               :can-be-null t)
-         (xmls:xmlrep-string-child it)))))
-
-(defmacro get-child-collections (address)
-  `(mapcar #'xmls:xmlrep-string-child
-           (parse-resulting-xml
-            (execute-query ,address (concatenate 'string "xmldb:get-child-collections('" ,address "')") :return-text t)
-            :multiple t)))
-
-(defmacro get-child-resources (address)
-  `(mapcar #'xmls:xmlrep-string-child
-           (parse-resulting-xml
-            (execute-query ,address (concatenate 'string "xmldb:get-child-resources('" ,address "')") :return-text t)
-            :multiple t)))
-
-(defmacro last-modified (address)
-  (with-gensyms (src document)
-    `(multiple-value-bind (,src ,document)
-         (divide-path ,address)
-       (xmls:xmlrep-string-child
-        (parse-resulting-xml
-         (execute-query ,address (concatenate 'string "xmldb:last-modified('" ,src "', '" ,document "')") :return-text t))))))
-
-(defmacro size (address)
-  (with-gensyms (src document)
-    `(multiple-value-bind (,src ,document)
-         (divide-path ,address)
-       (xmls:xmlrep-integer-child
-        (parse-resulting-xml
-         (execute-query ,address (concatenate 'string "xmldb:size('" ,src "', '" ,document "')") :return-text t))))))
-
-(defmacro reindex (address)
-  `(translate-boolean
-    (xmls:xmlrep-string-child
-     (parse-resulting-xml
-      (execute-query ,address (concatenate 'string "xmldb:reindex('" ,address "')") :return-text t)))))
-
-(defmacro get-document-owner (address)
-  (with-gensyms (src document)
-    `(multiple-value-bind (,src ,document)
-         (divide-path ,address)
-       (xmls:xmlrep-string-child
-        (parse-resulting-xml
-         (execute-query ,address (concatenate 'string "xmldb:get-owner('" ,src "', '" ,document "')") :return-text t))))))
-
-(defmacro get-collection-owner (address)
-  `(xmls:xmlrep-string-child
-    (parse-resulting-xml
-     (execute-query ,address (concatenate 'string "xmldb:get-owner('" ,address "')") :return-text t))))
-
-;;TODO:  authenticate(?)
-
+(test-interface-functions '(((create-collection "" "testcol") t)
+                            ((create-collection "testcol" "testcol2") t)
+                            ((get-collection-permissions "testcol") 493)
+                            ((put-document-from-string "testcol/testtest.xml" "text/plain" "<test>test</test>") t)
+                            ((get-document-permissions "testcol/testtest.xml") 420)
+                            ((document-has-lock "testcol/testtest.xml") nil)
+                            ((clear-lock "testcol/testtest.xml") nil)
+                            ((collection-available "testcol") t)
+                            ((get-child-collections "testcol") '("testcol2"))
+                            ((get-child-resources "testcol") '("testtest.xml"))
+                            ((size "testcol/testtest.xml") 17)
+                            ((reindex "testcol") t)
+                            ((get-document-owner "testcol/testtest.xml") (username *connection*))
+                            ((get-collection-owner "testcol/testcol2") (username *connection*))
+                            ((delete-from-db "testcol") t)))
 
 
